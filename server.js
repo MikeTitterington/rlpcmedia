67a4dc64e37f4446ecc527bfad50b37503ed482c
MikeTitterington <mt1051@plymouth.edu>
2021-06-20 13:54:06 -0400

    initial

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ app.use(express.static('public'));
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
   });
-app.listen(port, () =>
-console.log(Server is running on: http://localhost:${port}`));
+  app.listen(port, () =>
+  console.log("Server is running on: http://localhost:${port}"));
